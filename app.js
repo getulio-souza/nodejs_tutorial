bb8c1b27edf9b9d7eb4089b16f50b8fb496564ef
Getulio Souza <getuliosouza8@gmail.com>
2025-09-26 15:56:20 -0300

    first-file

---
+++ app.js
@@ -0,0 +1,12 @@
+// console.log('hello from node js')
+// console.log('hello from node js 2')
+
+//import module
+
+const readLine = require('readline');
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.question
