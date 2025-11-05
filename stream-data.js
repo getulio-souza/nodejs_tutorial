e696b242f38da5878ea21719735f8d0c58f51899
Getulio Souza <getuliosouza8@gmail.com>
2025-11-05 16:48:47 -0300

    feature: using-pipe-with-readable-stream

--- stream-data.js
+++ stream-data.js
@@ -19,11 +19,47 @@ server.listen(8000, '127.0.0.1', () => {
 // })
 
 
-server.on('request', (req, res) => {
-  let rs = fs.createReadStream('./files/large-file.txt');
+// server.on('request', (req, res)=> {
+//   fs.readFile('./files/large-file.txt', (err, data)=> {
+//     if(err){
+//       res.end('something went wrong');
+//       return;
+//     }else{
+//       res.end(data)
+//     }
+//   })
+// })
 
-  rs.on('data', (chunk) => {
-    res.write(chunk)
-    res.end(); 
-  })
+// server.on('request', (req, res)=> {
+//   let rs = fs.createReadStream('./files/large-file.txt');
+
+//   //in the callback function we receive the piece/chunk of data
+//   rs.on('data', (chunk)=> {
+//     res.write(chunk)
+//   })
+
+//   //now we use the end method because there is no more data 
+//   rs.on('end', ()=> {
+//     res.end();
+//   })
+
+//   rs.on('error', (err)=> {
+//     res.end(err)
+//   })
+
+// })
+
+//we use write because we are going to get the data piece by piece
+
+// if we use read, the first chunk of data will be arrived and no more data will be get
+
+
+// SOLUTION 3: USING PIPE METHOD
+
+server.on('request', (req, res)=> {
+  let rs = fs.createReadStream('./files/large-file.txt');
+  rs.pipe(res)
+
 })
+
+//pipe method is only avaliable in readble streams.
